d0d9e94206f0258d1200c96ce575eb686c58bf03
fsande <fsande@ull.es>
2026-04-26 17:55:23 +0100

    Code Updated

--- src/T4-Events-TS/30-present-callback/present.ts
+++ src/T4-Events-TS/30-present-callback/present.ts
@@ -11,24 +11,22 @@
  */
 
 /** 
- * @class
- * @description Represents a Present.
- *              Initially the present has an image that will be replaced
- *              by the present image when the original is clicked
+ * @classdesc Represents a Present.
+ *            Initially the present has an image that will be replaced
+ *            by the present image when the original is clicked
  */
 export class Present {
+  private image: HTMLImageElement;
   /*
    * @constructor
-   * @param {object} containerElement - DOM element that holds the present image
-   * @param {string} presentSrc - URL of the image to show when the present is opened
-   * @param {function} onOpenCallback - Callback to be invoked when the present is opened
+   * @param containerElement - DOM element that holds the present image
+   * @param presentSrc - URL of the image to show when the present is opened
+   * @param onOpenCallback - Callback to be invoked when the present is opened
    */
-  constructor(private containerElement: HTMLElement, private presentSrc: string, private onOpenCallback: () => void, private image?: HTMLImageElement) {
-    this.containerElement = containerElement;
-    this.presentSrc = presentSrc;
-    this.onOpenCallback = onOpenCallback;
-
-    // Create image and append to container.
+  constructor(private containerElement: HTMLElement, 
+              private presentSrc: string, 
+              private onOpenCallback: () => void) {
+    // Create image and append to container
     this.image = document.createElement('img');
     this.image.src = 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/1083533/gift-icon.png';
     this.image.addEventListener('click', this.openPresent);
@@ -36,9 +34,8 @@ export class Present {
   }
 
   /** 
-   * @method openPresent 
    * @description Event handler for click events
-   * @param {object} event - Event object
+   * @param event - Event object
    */
   private openPresent = (event: Event): void => {
     this.image!.src = this.presentSrc;
